8176fcf882fbf1e786aa407e39382b5ac06b4a95
Priyanka Shastri <priyankashastri01@gmail.com>
2024-02-04 00:37:30 +0530

    used map function to render table without extra state variable

--- src/app/student-names/page.js
+++ src/app/student-names/page.js
@@ -8,42 +8,41 @@ import StudentNameTable from '../../components/StudentNameTable'
 const StudentName = () => {
   const [text, setText] = useState()
   const [inputArray, setinputArray] = useState([])
-  const [tData, settData] = useState([])
 
   function onChange (e) {
       setText(e.target.value)
   }
 
+ 
   const onClickFunction = (e) => {
     const newInputArray = [...inputArray]
     newInputArray.push(text)
     setinputArray(newInputArray)
-    console.log (newInputArray) 
-    
-    const newTableData = [...tData]
-    newTableData.push( 
-    // key is given on html element (on outermost html element)or on component when array is going to be printed 
-      <tr key = {inputArray.length}>
-        <td>
-         {newInputArray.length}
-        </td>
-        <td>
-          {text}
-        </td>
-      </tr>
-    )
-
-   
-    settData(newTableData)
-
+    console.log (newInputArray)
   }
+
+  // passing a function as an argument of map function
+  const tableData = inputArray.map(
+    (newText, index) => {
+      return (
+        <tr key ={index}>
+          <td>
+           {index+1}
+          </td>
+          <td>
+           {newText}
+          </td>
+        </tr>
+      )
+    }
+  )
 
   return (
     <div>
       {/* can  also be written as onChange = { (e) => onChange(e)} */}
       <StudentNameInput placeholder = "Enter student's name" onChange={onChange} />
       <StudentNameButton onClickFunction = {onClickFunction} />
-      <StudentNameTable tableData = {tData}  />
+      <StudentNameTable tableData = {tableData}  />
     </div>
   )
 }
